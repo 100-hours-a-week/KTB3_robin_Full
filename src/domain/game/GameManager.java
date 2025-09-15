beea3a3b486aa139d9f53d6b79213745983417d1
Yongsu Choi <83999626+dragonwaterr@users.noreply.github.com>
2025-09-15 14:52:12 +0900

    refactor : 중복된 메소드호출 제거

--- src/domain/game/GameManager.java
+++ src/domain/game/GameManager.java
@@ -72,30 +72,21 @@ public class GameManager  {
             switch (number) { // number 에 맞게 상황 설명을 view 에서 출력
                 case 1:
                     outView.printOneVoneChanceSituation(player.getName(), player.getTeamName());
-                    // 각 역할군에 맞게 할 수 있는 행동들 나열
-                    availableActionNumbers = playerActionService.getAvailableActionNumbers(player, number);
-                    outView.printAvailableActions(availableActionNumbers, mappingService);
-                    handleActionInputLoop(availableActionNumbers, inputValidator, mappingService, playerActionService);
-                    // 행동의 결과를 결과표에 기록
                     break;
                 case 2:
                     outView.printNearByBoxSituation(player.getName(), player.getTeamName());
-                    availableActionNumbers = playerActionService.getAvailableActionNumbers(player, number);
-                    outView.printAvailableActions(availableActionNumbers, mappingService);
-                    handleActionInputLoop(availableActionNumbers, inputValidator, mappingService, playerActionService);
                     break;
                 case 3:
                     outView.printCounterattackSituation(player.getName(), player.getTeamName());
-                    availableActionNumbers = playerActionService.getAvailableActionNumbers(player, number);
-                    outView.printAvailableActions(availableActionNumbers, mappingService);
-                    handleActionInputLoop(availableActionNumbers, inputValidator, mappingService, playerActionService);
                     break;
                 default:
                     outView.printDefendSituation(player.getName(), player.getTeamName());
-                    availableActionNumbers = playerActionService.getAvailableActionNumbers(player, number);
-                    outView.printAvailableActions(availableActionNumbers, mappingService);
-                    handleActionInputLoop(availableActionNumbers, inputValidator, mappingService, playerActionService);
             }
+            // 각 역할군에 맞게 할 수 있는 행동들 나열
+            availableActionNumbers = playerActionService.getAvailableActionNumbers(player, number);
+            // 행동의 결과를 결과표에 기록
+            outView.printAvailableActions(availableActionNumbers, mappingService);
+            handleActionInputLoop(availableActionNumbers, inputValidator, mappingService, playerActionService);
         }
     }
 
