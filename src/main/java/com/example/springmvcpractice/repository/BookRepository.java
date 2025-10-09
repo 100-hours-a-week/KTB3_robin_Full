28f95a4282065056e34e50b11794e85208ef7bd1
dragonwaterr <yongsu626@naver.com>
2025-10-09 18:27:09 +0900

    feat : save 메소드 book.getId() 가 null 일 때만 새 id 부여하도록 수정

--- src/main/java/com/example/springmvcpractice/repository/BookRepository.java
+++ src/main/java/com/example/springmvcpractice/repository/BookRepository.java
@@ -11,13 +11,16 @@ public class BookRepository {
     long sequence = 0;
 
     public BookRepository() {
-        save(new BookDto(sequence++, "Clean Code", "Robert C. Martin", "소프트웨어 장인 정신을 담은 책입니다.", "9780132350884"));
-        save(new BookDto(sequence++, "객체지향의 사실과 오해", "조영호", "객체지향의 본질을 쉽게 설명합니다.", "9791186710770"));
-        save(new BookDto(sequence++, "Effective Java", "Joshua Bloch", "자바 개발자를 위한 베스트 프랙티스 모음집입니다.", "9780134685991"));
+        save(new BookDto(null, "Clean Code", "Robert C. Martin", "소프트웨어 장인 정신을 담은 책입니다.", "9780132350884"));
+        save(new BookDto(null, "객체지향의 사실과 오해", "조영호", "객체지향의 본질을 쉽게 설명합니다.", "9791186710770"));
+        save(new BookDto(null, "Effective Java", "Joshua Bloch", "자바 개발자를 위한 베스트 프랙티스 모음집입니다.", "9780134685991"));
     }
 
     BookDto save(BookDto book) {
         Long bookId = book.getId();
+        if(bookId == null) {
+            bookId = ++sequence;
+        }
         bookMap.put(bookId, book);
         return bookMap.get(bookId);
     }
