0b09001b2c1610bb72339ef0e7355440895d0269
dragonwaterr <yongsu626@naver.com>
2025-10-10 22:16:31 +0900

    test : 존재하지 않는 id로 페이지 요청 시 404 상태코드 반환 테스트 코드 추가

--- src/test/java/com/example/springmvcpractice/service/BookServiceTest.java
+++ src/test/java/com/example/springmvcpractice/service/BookServiceTest.java
@@ -3,8 +3,12 @@ package com.example.springmvcpractice.service;
 import com.example.springmvcpractice.repository.BookRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class BookServiceTest {
@@ -31,6 +35,23 @@ public class BookServiceTest {
 
         // then : 생성자로 주입한 mock이 실제로 호출되었는지 검증
         verify(mockRepo).findAll();
+        verifyNoMoreInteractions(mockRepo);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 id로 페이지 요청 시 404 응답 동작 테스트")
+    void notFoundIsReturnedForInvalidId() {
+        // given : id 1~3 까지만 저장된 상태
+        BookRepository mockRepo = mock(BookRepository.class);
+        when(mockRepo.findById(4L)).thenReturn(Optional.empty());
+        BookService bookService = new BookService(mockRepo);
+
+        // when
+        ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> bookService.getBookById(4L));
+
+        // then : 404 상태 코드 검증 + 상호작용 검증
+        assertEquals(HttpStatus.NOT_FOUND.value(), exception.getStatusCode().value(), "404 상태여야 합니다.");
+        verify(mockRepo).findById(4L);
         verifyNoMoreInteractions(mockRepo);
     }
 }
