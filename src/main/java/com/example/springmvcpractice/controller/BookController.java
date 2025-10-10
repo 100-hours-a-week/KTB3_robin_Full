23ce6903c7770bf76823336e33fe328af7871626
dragonwaterr <yongsu626@naver.com>
2025-10-10 22:34:43 +0900

    feat : 도서 컨트롤러 추가(도서 목록, 단건 조회 메소드)

---
+++ src/main/java/com/example/springmvcpractice/controller/BookController.java
@@ -0,0 +1,36 @@
+package com.example.springmvcpractice.controller;
+
+import com.example.springmvcpractice.dto.BookDto;
+import com.example.springmvcpractice.service.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public String getBooks(Model model) {
+        List<BookDto> bookList = bookService.getAllBooks();
+        model.addAttribute("books", bookList);
+        return "books/list";
+    }
+
+    @GetMapping("/{id}")
+    public String getSingleBook(Model model, @PathVariable Long id) {
+        BookDto bookDto = bookService.getBookById(id);
+        model.addAttribute("book", bookDto);
+        return "books/detail";
+    }
+}
